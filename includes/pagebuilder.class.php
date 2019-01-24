a79233b0b7d71e10b4d1b1c2afbe445fa207e178
udf <tabhooked@gmail.com>
2019-01-24 06:44:09 +0200

    favicon

--- includes/pagebuilder.class.php
+++ includes/pagebuilder.class.php
@@ -14,6 +14,7 @@ class Pagebuilder
     <!DOCTYPE html>
     <html>
     <head>
+        <link rel="shortcut icon" type="image/png" href="favicon.png">
         <title><?php echo $title ?> | Camagru</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="css/default.css">
